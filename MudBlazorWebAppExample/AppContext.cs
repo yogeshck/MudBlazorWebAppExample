e9ce0e9a1af801513202526c89ddf7ac96f9cf22
yogeshck <yogesh.jun5@gmail.com>
2024-08-29 22:36:32 +1000

    latest

---
+++ MudBlazorWebAppExample/AppContext.cs
@@ -0,0 +1,11 @@
+﻿using MudBlazorWebAppExample.Models;
+
+namespace MudBlazorWebAppExample
+{
+    public class ExampleAppContext
+    {
+        private static List<Order> _orders = new();
+
+        public static List<Order> Orders { get => _orders; set => _orders = value; }
+    }
+}
---
+++ MudBlazorWebAppExample/Models/Order.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+
+namespace MudBlazorWebAppExample.Models;
+
+public class Order
+{
+    public Order() { OrderLines = new(); }
+
+    public string OrderNumber { get; set; }
+    public DateTime OrderDate { get; set; }
+    public string ContactNumber { get; set; }
+    public string ContactName { get; set; }
+    public List<OrderLine> OrderLines { get; set; }
+}
+
+public class OrderValidator : AbstractValidator<Order>
+{
+    public OrderValidator() 
+    {
+        RuleFor(order => order.OrderNumber).NotEmpty();
+        RuleFor(order => order.OrderDate).NotEmpty();
+    }
+}
+
+public class OrderLine
+{
+    public string OrderLineId { get; set; }
+    public string ProductId { get; set; }
+    public decimal Price { get; set; }
+    public int Quantity { get; set; }
+}
